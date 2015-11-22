6025641c4df961bb6c0c3685970f05e4ea166f9f
undefinederror <nowtypethis@gmail.com>
2015-11-22 20:40:34 +0100

    stopwatch fixed references

--- atw/public/javascripts/Stopwatch.js
+++ atw/public/javascripts/Stopwatch.js
@@ -1,91 +1,92 @@
 ﻿var Stopwatch = (function () {
-    var t,
-        t1=0,
-        t2=0,
-        tot,
-        tick,
-        tIntId = null,
-        cbacks = {},
-        secs2Time = function (secs) {
-            var hours = Math.floor(secs / (60 * 60)),
-                minutes = Math.floor(secs / 60),
-                seconds = Math.floor(secs % 60),
-                obj = {
-                    "h": hours.toString().length === 1 ? '0' + hours : hours,
-                    "m": minutes.toString().length === 1 ? '0' +
-                        minutes : minutes,
-                    "s": seconds.toString().length === 1 ? '0' +
-                        seconds : seconds
-                };
-            return obj;
-        },
-        formatTime = function (timeObj) {
-            return timeObj.h + ':' + timeObj.m + ':' + timeObj.s;
-        },
-        cbackHandler = function (cback, name) {
-            if (typeof cback === 'function') {
-                cbacks[name] = cback;
-            }
-        },
-        calc = function () { t= tot - (t2 - t1); },
-        tIntFn= function () {
-            t2 = new Date().getTime();
-            calc();
-            if(cbacks.onTick) cbacks.onTick();
-            if (t <= 0) fnComplete();
-        },
-        fnComplete = function () {
-            proto.pause();
-            // invalidate resume, pause
-            if (cbacks.onComplete) cbacks.onComplete();
-        },
-        fnTick = function () { 
-            if (cbacks.onTick) cbacks.onTick();
-        }
-        tIntSet = function () {
-            tIntId = setInterval(tIntFn, tick);
-        },
-        proto = {
-            start: function () {
-                t = tot;
-                t1 = new Date().getTime();
-                tIntSet();
+    function create(argTot,argTick) {
+       
+        var t = argTot,
+            t1 = 0,
+            t2 = 0,
+            tot = t,
+            tick = argTick,
+            tIntId = null,
+            cbacks = {},
+            secs2Time = function (secs) {
+                var hours = Math.floor(secs / (60 * 60)),
+                    minutes = Math.floor(secs / 60),
+                    seconds = Math.floor(secs % 60),
+                    obj = {
+                        "h": hours.toString().length === 1 ? '0' + hours : hours,
+                        "m": minutes.toString().length === 1 ? '0' +
+                            minutes : minutes,
+                        "s": seconds.toString().length === 1 ? '0' +
+                            seconds : seconds
+                    };
+                return obj;
             },
-            pause: function () {
-                tIntId = clearTimeout(tIntId);
+            formatTime = function (timeObj) {
+                return timeObj.h + ':' + timeObj.m + ':' + timeObj.s;
+            },
+            cbackHandler = function (cback, name) {
+                if (typeof cback === 'function') {
+                    cbacks[name] = cback;
+                }
+            },
+            calc = function () { t = tot - (t2 - t1); },
+            tIntFn = function () {
                 t2 = new Date().getTime();
                 calc();
+                if (cbacks.onTick) cbacks.onTick();
+                if (t <= 0) fnComplete();
             },
-            resume: function () {
-                tot = t;
-                t1 = new Date().getTime();
-                tIntSet();
+            fnComplete = function () {
+                proto.pause();
+                // invalidate resume, pause
+                if (cbacks.onComplete) cbacks.onComplete();
             },
-            onTick: function (cback) {
-                cbackHandler(cback, 'onTick');
+            fnTick = function () {
+                if (cbacks.onTick) cbacks.onTick();
             },
-            onComplete: function (cback) {
-                cbackHandler(cback, 'onComplete');
+            tIntSet = function () {
+                tIntId = setInterval(tIntFn, tick);
+            },
+            proto = {
+                start: function () {
+                    t = tot;
+                    t1 = new Date().getTime();
+                    tIntSet();
+                },
+                pause: function () {
+                    tIntId = clearTimeout(tIntId);
+                    t2 = new Date().getTime();
+                    calc();
+                },
+                resume: function () {
+                    tot = t;
+                    t1 = new Date().getTime();
+                    tIntSet();
+                },
+                onTick: function (cback) {
+                    cbackHandler(cback, 'onTick');
+                },
+                onComplete: function (cback) {
+                    cbackHandler(cback, 'onComplete');
+                }
+            };
+        var propObj = {
+            time: {
+                get: function () {
+                    return formatTime(secs2Time((t / 1000) | 0));
+                },
+                set: function () {
+                    console.warn('yeah right...');
+                    return false;
+                }
             }
         };
-    var propObj = {
-        time: {
-            get: function () {
-                return formatTime(secs2Time((t / 1000) | 0));
-            },
-            set: function () {
-                console.warn('yeah right...');
-                return false;
-            }
-        }
-    };
+        return Object.create(proto, propObj);
+    }
     return {
         create: function (argTot, argTick) {
             // args in millisenconds
-            tot = argTot;
-            t = tot;
-            tick = argTick;
-            return Object.create(proto, propObj);
+            return create.apply(null,arguments)
         }
     }
 })();
